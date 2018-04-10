2c17640f6034b4cd9d223a0a231f11fb47fd6513
Igor Mendonça <igor.mendonca@zup.com.br>
2018-04-10 15:54:21 -0300

    Include unit tests to Example entity

--- poc-release/src/main/java/com/spring/boot/release/pocrelease/model/Example.java
+++ poc-release/src/main/java/com/spring/boot/release/pocrelease/model/Example.java
@@ -1,5 +1,6 @@
 package com.spring.boot.release.pocrelease.model;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Entity;
@@ -7,10 +8,12 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Builder
 @Getter
 public class Example {
 
     @Id
+    @NotEmpty
     private String id;
 
     @NotEmpty
---
+++ poc-release/src/test/groovy/com/spring/boot/release/pocrelease/service/ExampleServiceTest.groovy
@@ -0,0 +1,7 @@
+package com.spring.boot.release.pocrelease.service
+
+import spock.lang.Specification
+
+
+class ExampleServiceTest extends Specification {
+}
